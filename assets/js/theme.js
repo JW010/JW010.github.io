d12afed7c714d607beca86850a96f9f241de9668
Sarman Keshvala <sagarkeshwala15@gmail.com>
2023-02-06 19:36:30 +0530

    Fixed Scrolling issue on mobile

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -63,13 +63,13 @@
 	// Detect mobile device and add class "is-mobile" to </body>
 	// ==========================================================
 
-	// Detect mobile device (Do not remove!!!)
-	var isMobile =
-		/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Nokia|Opera Mini/i.test(
-			navigator.userAgent
-		)
-			? true
-			: false;
+	// Detect mobile device (Do not remove! !!)
+	var isMobile = false;
+		// /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Nokia|Opera Mini/i.test(
+		// 	navigator.userAgent
+		// )
+		// 	? true
+		// 	: false;
 
 	// Add class "is-mobile" to </body>
 	if (isMobile) {
